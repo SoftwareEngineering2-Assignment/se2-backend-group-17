0760ef7d4fdb9e797c0728177471c9aaade95438
leorizos <leorizos@gmail.com>
2023-02-22 21:51:58 +0200

    First try

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,11 +1,25 @@
+// /* eslint-disable no-console */
+// const {pipe, has, ifElse, assoc, identity, allPass, propEq} = require('ramda');
+
+// const withFormatMessageForProduction = ifElse(
+//   allPass([propEq('status', 500), () => process.env.NODE_ENV === 'production']),
+//   assoc('message', 'Internal server error occurred.'),
+//   identity
+// );
+
+// module.exports = (error, req, res, next) => 
+//   /**
+//      * @name error
+//      * @description Middleware that handles errors
+//      */
+//   pipe(
+//     (e) => ({...e, message: e.message}),
+//     ifElse(has('status'), identity, assoc('status', 500)),
+//     withFormatMessageForProduction,
+//     (fError) => res.status(fError.status).json(fError)
+//   )(error);
 /* eslint-disable no-console */
 const {pipe, has, ifElse, assoc, identity, allPass, propEq} = require('ramda');
-
-const withFormatMessageForProduction = ifElse(
-  allPass([propEq('status', 500), () => process.env.NODE_ENV === 'production']),
-  assoc('message', 'Internal server error occurred.'),
-  identity
-);
 
 module.exports = (error, req, res, next) => 
   /**
@@ -15,6 +29,10 @@ module.exports = (error, req, res, next) =>
   pipe(
     (e) => ({...e, message: e.message}),
     ifElse(has('status'), identity, assoc('status', 500)),
-    withFormatMessageForProduction,
+    ifElse(
+      allPass([propEq('status', 500), () => process.env.NODE_ENV === 'production']),
+      assoc('message', 'Internal server error occurred.'),
+      identity
+    ),
     (fError) => res.status(fError.status).json(fError)
   )(error);
